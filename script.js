516ea41d29c64c29d2b3bd3bbdd9b4932e30f180
Rusetskaya12 <rusetskaya.ek@gmail.com>
2023-02-16 17:41:44 +0300

    add time/date

---
+++ script.js
@@ -0,0 +1,55 @@
+"use strict"
+
+
+//отображать время на странице//
+function showTime () {
+    const date = new Date();
+    const currentTime = date.toLocaleTimeString();
+    const time = document.querySelector(".time");
+    time.textContent = currentTime;
+    setTimeout(showTime, 1000); 
+};
+showTime();
+
+
+function showDate () {
+   
+    const dateToday = new Date();
+    const options = {weekday: "long", month: 'long', day: 'numeric'};
+    const date = document.querySelector(".date");
+    const currentDate = dateToday.toLocaleDateString('en-US', options);  
+    date.textContent = currentDate;
+   
+};
+showDate();
+
+
+
+function setGreet() {
+    const greeting = document.querySelector('.greeting')
+    const today = new Date(),
+      hour = today.getHours()
+    if (hour > 18 || hour < 24) {
+      greeting.textContent = 'Good evening,  '
+    } else if (hour > 6 || hour < 12) {
+      greeting.textContent = 'Good morning,  '
+    } else if (hour > 12 || hous < 18) {
+      greeting.textContent = 'Good afternoon,  '
+    } else {
+      greeting.textContent = 'Good night,  '
+    }
+  };
+  setGreet();
+
+  function setLocalStorage() {
+    localStorage.setItem('name', name.value);
+  }
+  window.addEventListener('beforeunload', setLocalStorage);
+
+
+  function getLocalStorage() {
+    if(localStorage.getItem('name')) {
+      name.value = localStorage.getItem('name');
+    }
+  }
+  window.addEventListener('load', getLocalStorage);
